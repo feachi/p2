e30c914ec03dc8d12a99fec1673bf97399a1aec4
feachi <148525167+feachi@users.noreply.github.com>
2024-04-21 16:32:55 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a whole number:");
+        int number;
+        number = Convert.ToInt32(Console.ReadLine());
+        int divisors = 0;
+
+        for (int i = 1; i <= number; ++i)
+        {
+            if (number % i == 0)
+            {
+                divisors++;
+            }
+        }
+
+        if (divisors == 2)
+        {
+            Console.WriteLine("It`s a prime number!");
+        }
+        else
+        {
+            Console.WriteLine("It`s not a prime number");
+        }
+        Console.ReadKey();
+    }
+}
